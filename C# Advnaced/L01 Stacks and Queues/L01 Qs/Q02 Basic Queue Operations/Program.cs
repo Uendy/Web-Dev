9d99a947fc488f7b405c8f42ddf254bed6144084
Uendy <alex.beshkov98@gmail.com>
2020-12-24 19:25:58 +0200

    Update: -Finished Q02 Basic Queue Operations

--- C# Advnaced/L01 Stacks and Queues/L01 Qs/Q02 Basic Queue Operations/Program.cs
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q02 Basic Queue Operations/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Q02_Basic_Queue_Operations
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        // Reading input: 
+        var inputTokens = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        int enqueue = inputTokens[0];
+        int dequeue = inputTokens[1];
+        int find = inputTokens[2];
+
+        var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        // Initialize and Enqueue, Dequeue Queue
+        var queue = new Queue<int>();
+        for (int i = 0; i < enqueue && i < input.Count(); i++)
         {
+            queue.Enqueue(input[i]);
+        }
+
+        for (int i = 0; i < dequeue && queue.Any(); i++)
+        {
+            queue.Dequeue();
+        }
+
+        // Find and print depending
+        bool contains = queue.Contains(find);
+        if (contains)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            bool emptyQueue = !queue.Any();
+            if (emptyQueue)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
         }
     }
 }
