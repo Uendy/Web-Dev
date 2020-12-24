e117cbbe8a0d24d3807689ec2c9ea5172ff5141b
Uendy <alex.beshkov98@gmail.com>
2020-12-24 18:45:40 +0200

    Update: -Finished Q01 Basic Stack Operations

--- C# Advnaced/L01 Stacks and Queues/L01 S&Q Qs/Q01 Basic Stack Operations/Program.cs
+++ C# Advnaced/L01 Stacks and Queues/L01 S&Q Qs/Q01 Basic Stack Operations/Program.cs
@@ -5,10 +5,45 @@ public class Program
 {
     public static void Main()
     {
-        int push = int.Parse(Console.ReadLine());
-        int pop = int.Parse(Console.ReadLine());
-        int find = int.Parse(Console.ReadLine());
+        // Reading input and initializing stack:
+        var inputTokens = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        int push = inputTokens[0];
+        int pop = inputTokens[1];
+        int find = inputTokens[2];
 
+        var stack = new Stack<int>();
+
+        var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        // Pushing:
+        for (int i = 0; i < push && i < input.Count(); i++)
+        {
+            stack.Push(input[i]);
+        }
+
+        // Popping:
+        for (int i = 0; i < pop && stack.Any(); i++)
+        {
+            stack.Pop();
+        }
+
+        // Check for number and print output
+        bool contains = stack.Contains(find);
+        if (contains)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            if (stack.Any())
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
 
     }
 }
