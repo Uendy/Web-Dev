dda06c118e944103dcc16f770c05059b8d84635f
Uendy <alex.beshkov98@gmail.com>
2020-12-24 19:40:33 +0200

    Adding: -Finished: Q03 Maximum and Minimum Element

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q03 Maximum and Minimum Element/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        int numberOfInputs = int.Parse(Console.ReadLine());
+
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < numberOfInputs; i++)
+        {
+            var inputTokens = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int command = inputTokens[0];
+
+            // Depending on command manipulate stack:
+            switch (command)
+            {
+                case 1:
+                    int add = inputTokens[1];
+                    stack.Push(add);
+                    break;
+
+                case 2:
+                    if (stack.Any())
+                    {
+                        stack.Pop();
+                    }
+                    break;
+
+                case 3:
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                    break;
+
+                case 4:
+                    if (stack.Any())
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Print remaining stack:
+        bool empty = !stack.Any();
+        if (empty)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
