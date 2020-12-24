e3eb78a739ea6384c0e01c556fae90b8ff18d49d
Uendy <alex.beshkov98@gmail.com>
2020-12-24 19:44:27 +0200

    Adding: -Q04 Initial (need to go down to celebrate 24.12)

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q04 Fast Food/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        int food = int.Parse(Console.ReadLine());
+        var orders = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+    }
+}
