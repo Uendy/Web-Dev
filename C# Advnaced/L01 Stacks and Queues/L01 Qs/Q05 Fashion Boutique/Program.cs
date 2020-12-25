de0e10315bb13de71167f7afe70996ee6efb18d8
Uendy <alex.beshkov98@gmail.com>
2020-12-25 18:47:45 +0200

    Adding: -Finished Q05 Fashion Boutique

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q05 Fashion Boutique/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        var clothes = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToList());
+        var rackSize = int.Parse(Console.ReadLine());
+
+        var currentSum = 0;
+        var racksUsed = 1;
+
+        // Cycle and sum, while keeping track of racks used:
+        while (clothes.Any())
+        {
+            currentSum += clothes.Peek();
+
+            if (currentSum <= rackSize)
+            {
+                clothes.Pop();
+            }
+            else
+            {
+                currentSum = 0;
+                racksUsed++;
+            }
+        }
+
+        // Print output:
+        Console.WriteLine(racksUsed);
+    }
+}
