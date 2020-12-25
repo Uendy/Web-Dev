80705b1945d1cdf7fc8a8f204f79ba9e6e8a6b86
Uendy <alex.beshkov98@gmail.com>
2020-12-25 19:05:14 +0200

    Adding: -Finished: Q06 Songs Queue

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q06 Songs Queue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input and initialize queue:
+        var queue = new Queue<string>(Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).ToList());
+
+        // Read commands and execute:
+        while (queue.Any())
+        {
+            var commandTokens = Console.ReadLine().Split(' ').ToList();
+            string command = commandTokens[0];
+
+            switch (command)
+            {
+                case "Play":
+                    queue.Dequeue();
+                    break;
+
+                case "Add":
+                    // get full song and check
+                    string song = string.Join(" ", commandTokens.Skip(1));
+                    bool newSong = !queue.Contains(song);
+                    if (newSong)
+                    {
+                        queue.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    break;
+
+                case "Show":
+                    Console.WriteLine(string.Join(", ", queue));
+                    break;
+            }
+        }
+
+        // Print output:
+        Console.WriteLine("No more songs!");
+    }
+}
