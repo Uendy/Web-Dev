2defccd952b56376b8a34af8f6b9b3aede62a647
Uendy <alex.beshkov98@gmail.com>
2020-12-25 18:22:29 +0200

    Update: -Finished Q04 Fast Food

--- C# Advnaced/L01 Stacks and Queues/L01 Qs/Q04 Fast Food/Program.cs
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q04 Fast Food/Program.cs
@@ -5,8 +5,38 @@ public class Program
 {
     public static void Main()
     {
+        // Read input and initialize:
         int food = int.Parse(Console.ReadLine());
-        var orders = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var orders = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToList());
 
+        // Find biggest order
+        int biggestOrder = orders.Max();
+        Console.WriteLine(biggestOrder);
+
+        // Accept the orders (if possible)
+        foreach (var order in orders.ToList())
+        {
+            bool enoughFood = food >= order;
+            if (enoughFood)
+            {
+                food -= order;
+                orders.Dequeue();
+            }
+            else // even if you can fill more orders, apart from the one you can't you should stop
+            {
+                break;
+            }
+        }
+
+        // Depending on order print:
+        bool complete = !orders.Any();
+        if (complete)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        else
+        {
+            Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+        }
     }
 }
