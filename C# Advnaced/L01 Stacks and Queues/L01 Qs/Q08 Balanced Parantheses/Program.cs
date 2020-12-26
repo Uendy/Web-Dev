81878e9a2513b0bac8086dc1f128858ed0fe5de0
Uendy <alex.beshkov98@gmail.com>
2020-12-26 18:38:41 +0200

    Adding: -Q08 Balanced Parantheses (almost done, tried to be smart and check its validity, by checking if palindrome, but they are actually mirrors, so they are different symbols, when mirrored)

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q08 Balanced Parantheses/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        string input = Console.ReadLine();
+        var curveStack = new Stack<int>(); // (
+        var squareStack = new Stack<int>(); //[
+        var waveyStack = new Stack<int>();
+
+        // check for palendrome, otherwise the sequence is unbalanced:
+        bool isPalendrome = CheckPalendrome(input);
+        if (!isPalendrome)
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+
+        // Cycle through input chars and depending on currenct char, update the corresponding stack:
+        var inputAsArray = input.ToCharArray();
+        for (int i = 0; i < inputAsArray.Count(); i++)
+        {
+            char current = inputAsArray[i];
+
+            switch (current)
+            {
+                case '(':
+                    curveStack.Push(i);
+                    break;
+
+                case ')':
+                    CheckAndExecute(curveStack);
+                    break;
+
+                case '[':
+                    squareStack.Push(i);
+                    break;
+
+                case ']':
+                    CheckAndExecute(squareStack);
+                    break;
+
+                case '{':
+                    waveyStack.Push(i);
+                    break;
+
+                case '}':
+                    CheckAndExecute(waveyStack);
+                    break;
+            }
+        }
+
+        bool finalCheck = !curveStack.Any() || !squareStack.Any() || waveyStack.Any();
+        if (finalCheck)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+
+    public static bool CheckPalendrome(string input)
+    {
+        return input.SequenceEqual(input.Reverse());
+    }
+
+    public static void CheckAndExecute(Stack<int> stack) // Check for the element in its given stack and if there remove its partner or print fail
+    {
+        bool contains = stack.Any();
+        if (contains)
+        {
+            stack.Pop();
+        }
+        else // shut it down
+        {
+            Console.WriteLine("NO");
+            Environment.Exit(0); 
+        }
+    }
+}
---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q08 Balanced Parantheses/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Q08 Balanced Parantheses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Q08 Balanced Parantheses")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d47f6826-8339-4b0b-9ae7-90f6eb1e98d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
