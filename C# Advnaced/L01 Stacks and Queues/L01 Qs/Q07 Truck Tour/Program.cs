999b5d387c597efdc939ee7fb89237c1745a3dbb
Uendy <alex.beshkov98@gmail.com>
2020-12-26 17:08:25 +0200

    Adding: -Q07 Truck Tour (Start)

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q07 Truck Tour/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading and initializing:
+        var stations = int.Parse(Console.ReadLine());
+        var petrol = new Queue<int>();
+        var distance = new Queue<int>();
+
+        for (int i = 0; i < stations; i++)
+        {
+            var stationData = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            petrol.Enqueue(stationData[0]);
+            distance.Enqueue(stationData[1]);
+        }
+
+        int currentFuel = 0;
+    }
+}
