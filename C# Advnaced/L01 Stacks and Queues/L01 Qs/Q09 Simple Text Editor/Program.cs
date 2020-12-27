841762c10b8c28e883fa9c6baed83120787b87f3
Uendy <alex.beshkov98@gmail.com>
2020-12-27 18:05:52 +0200

    Update: -Done with all tasks, except 4 (hardest)

--- C# Advnaced/L01 Stacks and Queues/L01 Qs/Q09 Simple Text Editor/Program.cs
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q09 Simple Text Editor/Program.cs
@@ -23,21 +23,53 @@ public class Program
             switch (task)
             {
                 case "1":
-                    //
+                    // 1 someString - appends someString to the end of the text
+                    var someString = currentTask[1].ToCharArray();
+                    list = AddSomeString(list, someString);
                     break;
 
                 case "2":
-                    //
+                    // 2 count - erases the last count elements from the text
+                    int count = int.Parse(currentTask[1]);
+                    list = ListEraseCount(list, count);
                     break;
 
                 case "3":
-                    //
+                    // 3 index - returns the element at position index from the text
+                    int index = int.Parse(currentTask[1]);
+                    Console.WriteLine(list[index - 1]);
                     break;
 
                 case "4":
-                    //
+                    // 4 - undoes the last not undone command of type 1 / 2 and returns the text to the state before that operation
+                    // This will be tricky, can I add 1/2 commands to a queue and return?
                     break;
             }
+            tasks.Dequeue();
         }
+    }
+
+    public static List<string> AddSomeString(List<string> list, char[] someString)
+    {
+        foreach (var item in someString)
+        {
+            list.Add(item.ToString());
+        }
+        return list;
+    }
+
+    public static List<string> ListEraseCount(List<string> list, int count)
+    {
+        bool enoughElements = list.Count() > count;
+        if (enoughElements)
+        {
+            int indexToRemove = list.Count() - 3;
+            list.RemoveRange(indexToRemove, count);
+        }
+        else
+        {
+            list.Clear();
+        }
+        return list;
     }
 }
