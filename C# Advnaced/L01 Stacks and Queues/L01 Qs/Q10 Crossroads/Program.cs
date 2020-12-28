98bdd5e0a533781a0830c5c54b08ca44e9695b01
Uendy <alex.beshkov98@gmail.com>
2020-12-28 16:12:42 +0200

    Update: -Finished Q10 Crossroads (still not at 100%)

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q10 Crossroads/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading time and initializing queue:
+        int greenTime = int.Parse(Console.ReadLine());
+        int green = greenTime;
+        int freeTime = int.Parse(Console.ReadLine());
+        int free = freeTime;
+
+        var line = new Queue<string>();
+
+        int carsPassed = 0;
+
+        string input = Console.ReadLine();
+        while (input != "END")
+        {
+            if (input == "green") // reset the time we have
+            {
+                greenTime = green;
+                freeTime = free;
+            }
+            else
+            {
+                line.Enqueue(input);
+
+                // Begin releasing cars:
+                while (line.Any() && greenTime != 0)
+                {
+                    int carLength = line.Peek().Length;
+
+                    bool carCrosses = carLength <= greenTime;
+                    if (carCrosses) // it passes safely
+                    {
+                        carsPassed++;
+                        greenTime -= carLength;
+                    }
+                    else  // not enough greentime, try adding freetime:
+                    {
+                        int remainingTime = freeTime + greenTime;
+                        if (carLength <= freeTime + greenTime)
+                        {
+                            carsPassed++;
+                            greenTime = 0;
+                            freeTime = remainingTime - carLength;
+                        }
+                        else // crash:
+                        {
+                            var car = line.Peek().ToArray();
+                            char crashChar = car[remainingTime];
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{line.Peek()} was hit at {crashChar}.");
+                            return;
+                        }
+
+                    }
+                    line.Dequeue();
+                }
+            }
+            // continue reading input:
+            input = Console.ReadLine();
+        }
+        // If all passed then write success and how many cars passed:
+        Console.WriteLine("Everyone is safe.");
+        Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
+    }
+}
