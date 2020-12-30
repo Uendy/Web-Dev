5e71c97ceeb82e477f5fcf30304a930bf442560b
Uendy <alex.beshkov98@gmail.com>
2020-12-30 14:43:00 +0200

    Adding: -Q12 Cups and Bottles (Start schema)

---
+++ C# Advnaced/L01 Stacks and Queues/L01 Qs/Q12 Cups and Bottles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        var bottles = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+        var cups = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+        int wastedWater = 0;
+
+        // Begin pouring water:
+        while (bottles.Any() && cups.Any())
+        {
+            int currentBottle = bottles.Peek();
+            int currentCup = cups.Peek();
+
+            // Need to get the logic of the question
+            bool cupIsEnough = currentCup >= currentBottle;
+            if (cupIsEnough)
+            {
+                //int waterWasted = currentBottle - currentCup;
+            }
+            else
+            { 
+            
+            }
+        }
+
+        // Depending on empty bottles or empty cups print output:
+        if (bottles.Any())
+        {
+            Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+        }
+        else // Print remaining cups
+        {
+            Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+        }
+        Console.WriteLine($"Wasted litters of water: {wastedWater}");
+    }
+}
