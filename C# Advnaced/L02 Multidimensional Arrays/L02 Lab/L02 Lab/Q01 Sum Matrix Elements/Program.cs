2884bd0148b9947aa1630781f8d3020230a0867a
Uendy <alex.beshkov98@gmail.com>
2021-01-01 20:57:41 +0200

    Adding: -L02 Lab folder: -- L02 Lab.sln -- Q01 Sum Matrix Elements (Finished)

---
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Lab/L02 Lab/Q01 Sum Matrix Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input about paramaters:
+        var paramaters = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToList();
+        int rows = paramaters[0];
+        int cols = paramaters[1];
+
+        // Printing params:
+        Console.WriteLine(rows);
+        Console.WriteLine(cols);
+
+        // Recreate matrix:
+        var matrix = new int[rows, cols];
+        for (int row = 0; row < rows; row++)
+        {
+            var currentRow = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToList();
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = currentRow[col];
+            }
+        }
+
+        // Sum matrix:
+        long sum = 0;
+        foreach (var element in matrix)
+        {
+            sum += element;
+        }
+
+        // Printing sum:
+        Console.WriteLine(sum);
+    }
+}
