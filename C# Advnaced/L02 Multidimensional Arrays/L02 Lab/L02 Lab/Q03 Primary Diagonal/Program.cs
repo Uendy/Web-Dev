883dd3103ef36889e1bef8dfa400ae32eeb00d43
Uendy <alex.beshkov98@gmail.com>
2021-01-02 20:20:30 +0200

    Adding: -Finished: Q03 Primary Diagonal

---
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Lab/L02 Lab/Q03 Primary Diagonal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading parameter size:
+        int size = int.Parse(Console.ReadLine());
+
+        // Initialize matrix and begin reading elements and placing them in matrix:
+        var matrix = new int[size, size];
+        for (int rows = 0; rows < size; rows++)
+        {
+            var currentInput = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int cols = 0; cols < currentInput.Count(); cols++)
+            {
+                matrix[rows, cols] = currentInput[cols];
+            }
+        }
+
+        // Cycle matrix and sum diagonals:
+        int sum = 0;
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                bool diagonal = row == col;
+                if (diagonal)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+        }
+
+        // Printing sum:
+        Console.WriteLine(sum);
+    }
+}
---
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Lab/L02 Lab/Q03 Primary Diagonal/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Q03 Primary Diagonal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Q03 Primary Diagonal")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b34c4c2d-281d-4518-824e-14f56707388a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
