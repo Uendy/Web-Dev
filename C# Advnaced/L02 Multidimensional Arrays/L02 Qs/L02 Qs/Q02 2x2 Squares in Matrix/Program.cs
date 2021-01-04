12e5472e6f37a4c745ae843384a5d03e4ee3b793
Uendy <alex.beshkov98@gmail.com>
2021-01-04 20:48:21 +0200

    Adding: -Q02 2x2 Squares in Matrix (Finished)

---
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q02 2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading size of matrix:
+        var size = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var rows = size[0];
+        var cols = size[1];
+
+        // Initializing matrix and fill:
+        var matrix = new char[rows, cols];
+        for (int row = 0; row < rows; row++)
+        {
+            var rowElements = Console.ReadLine().Split(' ').Select(char.Parse).ToList();
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = rowElements[col];
+            }
+        }
+
+        // Find how many equal squares there are:
+        int squares = 0;
+        for (int row = 0; row < rows-1; row++)
+        {
+            for (int col = 0; col < cols-1; col++)
+            {
+                // Get all chars in current square:
+                var currentChar = matrix[row, col];
+                var aboveChar = matrix[row + 1, col];
+                var rightChar = matrix[row, col + 1];
+                var diagChar = matrix[row + 1, col + 1];
+
+                bool isSquare = 
+                    currentChar == aboveChar &&
+                    aboveChar == rightChar &&
+                    rightChar== diagChar;
+                if (isSquare)
+                {
+                    squares++;
+                }
+            }
+        }
+
+        // Printing number of squares:
+        Console.WriteLine(squares);
+    }
+}
