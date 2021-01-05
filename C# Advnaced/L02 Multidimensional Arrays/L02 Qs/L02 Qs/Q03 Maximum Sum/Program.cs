d9503cba8e665acbe33ea5f4cd331f0bd080cb96
Uendy <alex.beshkov98@gmail.com>
2021-01-05 21:38:35 +0200

    Adding: -Q03 V2 (Finished) // I redid the whole question using [][] instead of [,] just to find that my error was in not properly splitting the input with: - Split(' ') instead of: - Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries

--- C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q03 Maximum Sum/Program.cs
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q03 Maximum Sum/Program.cs
@@ -22,38 +22,32 @@ public class Program
 
         // find largest 3x3 sum:
         var maxSum = 0;
-        var currRow = 0;
-        var currCol = 0;
+        var rowIndex = 0;
+        var colIndex = 0;
 
         for (int row = 0; row < rows - 2; row++)
         {
             for (int col = 0; col < cols - 2; col++)
             {
-                var currentSum = matrix[row, col] +
-                    matrix[row + 1, col] +
-                    matrix[row + 2, col] +
-                    matrix[row, col + 1] +
-                    matrix[row, col + 2] +
-                    matrix[row + 1, col + 1] +
-                    matrix[row + 1, col + 2] +
-                    matrix[row + 2, col + 1] +
-                    matrix[row + 2, col + 2];
+                var currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                    + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                    + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
 
                 bool newHighestSum = currentSum > maxSum;
                 if (newHighestSum)
                 {
                     maxSum = currentSum;
-                    currRow = row;
-                    currCol = col;
+                    rowIndex = row;
+                    colIndex = col;
                 }
             }
         }
 
         // Print out sum and the 3x3 matrix:
         Console.WriteLine($"Sum = {maxSum}");
-        for (int row = currRow; row < currRow + 3; row++)
+        for (int row = rowIndex; row <= rowIndex + 2; row++)
         {
-            for (int col = currCol; col < currCol + 3; col++)
+            for (int col = colIndex; col <= colIndex + 2; col++)
             {
                 Console.Write($"{matrix[row, col]} ");
             }
---
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q03 V2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input parameters of matrix:
+        var size = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var rows = size[0];
+        var cols = size[1];
+
+        // Initialize and fill matrix:
+        var matrix = new int[rows][];
+        for (int row = 0; row < rows; row++)
+        {
+            matrix[row] = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        }
+
+        // Find biggest 3x3 sum:
+        var maxSum = 0;
+        var rowIndex = 0;
+        var colIndex = 0;
+
+        for (int row = 0; row < rows - 2; row++)
+        {
+            for (int col = 0; col < cols - 2; col++)
+            {
+                int sum = matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2]
+                    + matrix[row + 1][col] + matrix[row + 1][col + 1] + matrix[row + 1][col + 2]
+                    + matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2];
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    rowIndex = row;
+                    colIndex = col;
+                }
+            }
+        }
+
+        // Printing sum and 3x3 matrix:
+        Console.WriteLine($"Sum = {maxSum}");
+        for (int row = rowIndex; row <= rowIndex + 2; row++)
+        {
+            for (int col = colIndex; col <= colIndex + 2; col++)
+            {
+                Console.Write($"{matrix[row][col]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
