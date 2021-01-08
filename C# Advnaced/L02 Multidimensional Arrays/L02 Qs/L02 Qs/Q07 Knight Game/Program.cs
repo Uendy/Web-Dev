41b271ad9684a4c14e81b05c9a443182c17c5d06
Uendy <alex.beshkov98@gmail.com>
2021-01-08 17:10:19 +0200

    Adding: -Q07 Knight Game (start) // getting a basic feel for the question

---
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q07 Knight Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading size parameters:
+        var size = int.Parse(Console.ReadLine());
+
+        // Initialize and fill matrix:
+        var board = new char[size, size];
+        for (int row = 0; row < size; row++)
+        { 
+            var currentRow = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+            for (int col = 0; col < size; col++)
+            {
+                board[row,col] = currentRow[col];
+            }
+        }
+
+        // Need to cycle board, and find all K's, then make a method for seeing if it attacks knights,
+        // and to find a way to see which of the two knights is more dangerous and remove that one (looking for minimum knights to be removes)
+    }
+}
