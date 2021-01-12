c017eebf6f162b0335ed5e09352971cbcbef49c3
Uendy <alex.beshkov98@gmail.com>
2021-01-12 19:01:26 +0200

    Update: -Finished Q09 Miner

--- C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q09 Miner/Program.cs
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q09 Miner/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 public class Program
 {
@@ -41,18 +40,69 @@ public class Program
         while (commands.Any())
         {
             // Move:
+            var newRow = currentRow;
+            var newCol = currentCol;
+
+            var move = commands[0];
+            switch (move)
+            {
+                case "up":
+                    newRow -= 1;
+                    break;
+
+                case "right":
+                    newCol += 1;
+                    break;
+
+                case "down":
+                    newRow += 1;
+                    break;
+
+                case "left":
+                    newCol -= 1;
+                    break;
+                default:
+                    break;
+            }
 
             // if invalid inded, skip command
+            bool validIndex = newRow >= 0 && newRow < size
+                && newCol >= 0 && newCol < size;
+            if (validIndex)
+            {
+                // Update: row and col:
+                currentRow = newRow;
+                currentCol = newCol;
 
-            // if coal: pick it up and replace with '*'
-
-                // if no more coal print and end program
-
-            // reached end 'e', print and end the program
-
+                // if coal: pick it up and replace with '*'
+                bool foundCoal = matrix[currentRow, currentCol] == 'c';
+                if (foundCoal)
+                {
+                    matrix[currentRow, currentCol] = '*';
+                    coalRemaining--;
+                    
+                    // if no more coal print and end program
+                    bool noMoreCoal = coalRemaining == 0;
+                    if (noMoreCoal)
+                    {
+                        Console.WriteLine($"You collected all coals! ({currentRow}, {currentCol})");
+                        return;
+                    }
+                }
+                
+                // check if you reached end 'e', print and end the program
+                bool foundEnd = matrix[currentRow, currentCol] == 'e';
+                if (foundEnd)
+                {
+                    Console.WriteLine($"Game over! ({currentRow}, {currentCol})");
+                    return;
+                }
+            }
+            // Next command:
             commands.RemoveAt(0);
         }
 
         // If no more commands: print and end program:
+        Console.WriteLine($"{coalRemaining} coals left. ({currentRow}, {currentCol})");
     }
 }
