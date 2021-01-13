2759b73378bb12ff803634332ab7e88975ee2a48
Uendy <alex.beshkov98@gmail.com>
2021-01-13 20:54:24 +0200

    Update: -Finished Q10, I'm not that happy with the code I wrote, if I could make a list of 2D array holding the coordinates it would be much cleaner, so I might try and re-write it cleaner

--- C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q10 Radioactive Mutant Vampire Bunnies/Program.cs
+++ C# Advnaced/L02 Multidimensional Arrays/L02 Qs/L02 Qs/Q10 Radioactive Mutant Vampire Bunnies/Program.cs
@@ -17,7 +17,7 @@ public class Program
         var matrix = new char[rows, cols];
         for (int row = 0; row < rows; row++)
         {
-            var currentRow = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+            var currentRow = Console.ReadLine().ToCharArray().ToArray();
             for (int col = 0; col < cols; col++)
             {
                 matrix[row, col] = currentRow[col];
@@ -61,6 +61,45 @@ public class Program
                     break;
             }
 
+            // move player to new position:
+            matrix[playerRow, playerCol] = '.';
+
+            // multiply bunnies: (this is tough as I dont want to multiply then, as I cycle I find a bunny that I just added and multiply it again, or from the first multiplication will recouse and fill the whole matrix)
+            // What if I make a list of all B's, then cycle each one and find their neighbours, check each neighbour if legit, then convert.
+            var bunnies = new List<int>();
+            // cycle through the whole matrix and find all bunnies or just the ones, who dont have all bunny true neighbours, to not cycle so much:
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    bool isBunny = matrix[row, col] == 'B';
+                    if (isBunny)
+                    {
+                        bunnies.Add(row);
+                        bunnies.Add(col);
+                    }
+                }
+            }
+
+            // Cycle and find all valid neighbouring bunnies:
+            var newBunnies = new List<int>();
+            for (int i = 0; i < bunnies.Count(); i += 2)
+            {
+                var bunnyRow = bunnies[i];
+                var bunnyCol = bunnies[i + 1];
+
+                newBunnies = FindNeighbours(bunnyRow, bunnyCol, newBunnies, rows, cols);
+            }
+
+            // Find cycle through newBunnies and update matrix:
+            for (int i = 0; i < newBunnies.Count(); i += 2)
+            {
+                var bunnyRow = newBunnies[i];
+                var bunnyCol = newBunnies[i + 1];
+
+                matrix[bunnyRow, bunnyCol] = 'B';
+            }
+
             // See if valid or player is outside:
             bool playerLeft = newRow < 0
                 || newRow >= rows
@@ -69,57 +108,26 @@ public class Program
             if (playerLeft)// Print and end program
             {
                 // Print matrix and output and end:
-                PrintMatrix(matrix);
+                PrintMatrix(matrix, rows, cols);
                 Console.WriteLine($"won: {playerRow} {playerCol}");
                 return;
             }
-            else
+
+            // If player hasn't left, check bunnies:
+            playerRow = newRow;
+            playerCol = newCol;
+
+            // Check if he stepped on a bunny:
+            bool steppedOnBunny = matrix[playerRow, playerCol] == 'B';
+            if (steppedOnBunny)
             {
-                // move player to new position:
-                matrix[playerRow, playerCol] = '.';
-                playerRow = newRow;
-                playerCol = newCol;
-
-                // multiply bunnies: (this is tough as I dont want to multiply then, as I cycle I find a bunny that I just added and multiply it again, or from the first multiplication will recouse and fill the whole matrix)
-                // What if I make a list of all B's, then cycle each one and find their neighbours, check each neighbour if legit, then convert.
-                var bunnies = new List<int>();
-                // cycle through the whole matrix and find all bunnies or just the ones, who dont have all bunny true neighbours, to not cycle so much:
-                for (int row = 0; row < rows; row++)
-                {
-                    for (int col = 0; col < cols; col++)
-                    {
-                        bool isBunny = matrix[row, col] == 'B';
-                        if (isBunny)
-                        {
-                            bunnies.Add(row);
-                            bunnies.Add(col);
-                        }
-                    }
-                }
-
-                // Cycle and find all valid neighbouring bunnies:
-                var newBunnies = new List<int>();
-                for (int i = 0; i < bunnies.Count(); i += 2)
-                {
-                    var bunnyRow = bunnies[i];
-                    var bunnyCol = bunnies[i + 1];
-
-                    newBunnies = FindNeighbours(bunnyRow, bunnyCol, newBunnies, rows, cols);
-                }
-
-                // Find cycle through newBunnies and update matrix:
-
-
-                // Check if he stepped on a bunny:
-                bool steppedOnBunny = matrix[playerRow, playerCol] == 'B';
-                if (steppedOnBunny)
-                {
-                    // Print matrix and output: 
-                    PrintMatrix(matrix);
-                    Console.WriteLine($"dead: {playerRow} {playerCol}");
-                    return;
-                }
+                // Print matrix and output: 
+                PrintMatrix(matrix, rows, cols);
+                Console.WriteLine($"dead: {playerRow} {playerCol}");
+                return;
             }
+
+
 
             // Continue to next command:
             commands.RemoveAt(0);
@@ -171,11 +179,15 @@ public class Program
         return isValid;
     }
 
-    public static void PrintMatrix(char[,] matrix)
+    public static void PrintMatrix(char[,] matrix, int rows, int cols)
     {
-        foreach (var row in matrix)
+        for (int row = 0; row < rows; row++)
         {
-            Console.WriteLine(string.Join(" ", row));
+            for (int col = 0; col < cols; col++)
+            {
+                Console.Write($"{matrix[row, col]} ");
+            }
+            Console.WriteLine();
         }
     }
 }
