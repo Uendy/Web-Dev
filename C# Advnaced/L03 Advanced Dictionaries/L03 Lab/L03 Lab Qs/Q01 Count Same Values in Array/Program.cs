3616eec6f174f712f46e0010020ae1d07705f64d
Uendy <alex.beshkov98@gmail.com>
2021-01-15 16:02:25 +0200

    Update: -Finished Lab Q01 Count Same Values in Array

--- C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q01 Count Same Values in Array/Program.cs
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q01 Count Same Values in Array/Program.cs
@@ -5,5 +5,26 @@ public class Program
 {
     public static void Main()
     {
+        // Reading input:
+        var array = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToArray();
+
+        // Initialize and fill dictionary:
+        var dict = new Dictionary<decimal, int>(); // Key = decimal number, Value = count of occurances
+        foreach (var number in array)
+        {
+            // If not in dict, add it:
+            bool newNumber = !dict.ContainsKey(number);
+            if (newNumber)
+            {
+                dict[number] = 0;
+            }
+            dict[number]++;
+        }
+
+        // Print results
+        foreach (var num in dict)
+        {
+            Console.WriteLine($"{num.Key} - {num.Value} times");
+        }
     }
 }
