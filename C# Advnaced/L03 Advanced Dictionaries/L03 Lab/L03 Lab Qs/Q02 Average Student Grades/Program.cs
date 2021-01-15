b0014f312477db3a51a0c43d42990a25d1d2d90f
Uendy <alex.beshkov98@gmail.com>
2021-01-15 19:17:04 +0200

    Adding: -Finished: Q03 Product Shop

---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q02 Average Student Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Initialize dict:
+        var grades = new Dictionary<string, List<decimal>>(); // Key = studentName, Value = List<decimal>, of grades
+
+        // Reading inputs:
+        int inputCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < inputCount; i++)
+        {
+            var input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var studentName = input[0];
+            var grade = decimal.Parse(input[1]);
+
+            // Update grades:
+            bool newStudent = !grades.ContainsKey(studentName);
+            if (newStudent)
+            {
+                grades[studentName] = new List<decimal>();
+            }
+            grades[studentName].Add(grade);
+        }
+
+        // Formatting, Averaging and Printing:
+        foreach (var student in grades)
+        {
+            Console.Write($"{student.Key} -> ");
+            var allGrades = string.Join(" ", student.Value.Select(x => x.ToString("F2")));
+            Console.WriteLine($"{allGrades} (avg: {student.Value.Average():f2})");
+        }
+    }
+}
---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q03 Product Shop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Initialize dicts:
+        var catalogue = new SortedDictionary<string, Dictionary<string, double>>(); // Key = store name, Value = nestedDict: key = product, value = price
+
+        // Begin reading input and updating dict:
+        string input = Console.ReadLine();
+        while (input != "Revision")
+        {
+            var inputElements = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var shopName = inputElements[0];
+            var productName = inputElements[1];
+            var price = double.Parse(inputElements[2]);
+
+            bool newStore = !catalogue.ContainsKey(shopName);
+            if (newStore)
+            {
+                catalogue[shopName] = new Dictionary<string, double>();
+            }
+
+            catalogue[shopName][productName] = price;
+
+            // Continue reading input:
+            input = Console.ReadLine();
+        }
+
+        // Print out shops and products: 
+        foreach (var shop in catalogue)
+        {
+            Console.WriteLine($"{shop.Key}->");
+            foreach (var item in shop.Value)
+            {
+                Console.WriteLine($"Product: {item.Key}, Price:{item.Value} ");
+            }
+        }
+    }
+}
