8dfa2da3862a1e511e88cc71f73eaded95e1b05b
Uendy <alex.beshkov98@gmail.com>
2021-01-16 11:23:03 +0200

    Adding: -Finished: Q04 Cities by Continent and Country

--- C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q03 Product Shop/Program.cs
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q03 Product Shop/Program.cs
@@ -12,11 +12,13 @@ public class Program
         string input = Console.ReadLine();
         while (input != "Revision")
         {
+            // Extracting info from input:
             var inputElements = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
             var shopName = inputElements[0];
             var productName = inputElements[1];
             var price = double.Parse(inputElements[2]);
 
+            // Initializing new nested dict if the shop is new:
             bool newStore = !catalogue.ContainsKey(shopName);
             if (newStore)
             {
@@ -35,7 +37,7 @@ public class Program
             Console.WriteLine($"{shop.Key}->");
             foreach (var item in shop.Value)
             {
-                Console.WriteLine($"Product: {item.Key}, Price:{item.Value} ");
+                Console.WriteLine($"Product: {item.Key}, Price: {item.Value} ");
             }
         }
     }
---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Lab/L03 Lab Qs/Q04 Cities by Continent and Country/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading inputCount
+        var inputCount = int.Parse(Console.ReadLine());
+
+        // Initialize dict: 
+        var map = new Dictionary<string, Dictionary<string, List<string>>>(); // key = continent, value = Dict<key = country, value = list of cities>:
+
+        // Read inputs and update dict:
+        for (int i = 0; i < inputCount; i++)
+        {
+            var inputInfo = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var continent = inputInfo[0];
+            var country = inputInfo[1];
+            var city = inputInfo[2];
+
+            // Begin updating dict:
+            if (!map.ContainsKey(continent))
+            {
+                map[continent] = new Dictionary<string, List<string>>();
+            }
+            if (!map[continent].ContainsKey(country))
+            {
+                map[continent][country] = new List<string>();
+            }
+
+            map[continent][country].Add(city);
+        }
+
+        // Print map:
+        foreach (var continent in map)
+        {
+            Console.WriteLine($"{continent.Key}: ");
+            foreach (var country in continent.Value)
+            {
+                Console.WriteLine($"{country.Key} -> {string.Join(" ", country.Value)}");
+            }
+        }
+    }
+}
