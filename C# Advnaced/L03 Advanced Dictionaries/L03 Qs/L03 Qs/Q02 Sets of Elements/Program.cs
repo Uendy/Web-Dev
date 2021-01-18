90086f1c19eabd3998bc7bfbd1a0d435ea98ec0d
Uendy <alex.beshkov98@gmail.com>
2021-01-18 11:47:16 +0200

    Adding: -Finished: Q02 Sets of Elements

---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Qs/L03 Qs/Q02 Sets of Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading parameters: 
+        var parameters = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        
+        // Initiliaze and fill first set:
+        var firstSet = new HashSet<int>();
+        for (int i = 0; i < parameters[0]; i++)
+        {
+            firstSet.Add(int.Parse(Console.ReadLine()));
+        }
+
+        // Initialize and fill second set:
+        var secondSet = new HashSet<int>();
+        for (int i = 0; i < parameters[1]; i++)
+        {
+            secondSet.Add(int.Parse(Console.ReadLine()));
+        }
+
+        // Find numbers that are in both sets:
+        var numsInBothSets = new List<int>();
+
+        foreach (var num in firstSet)
+        {
+            bool inBothSets = secondSet.Contains(num);
+            if (inBothSets)
+            {
+                numsInBothSets.Add(num);
+            }
+        }
+
+        // Printing all those numbers:
+        Console.WriteLine(string.Join(" ", numsInBothSets));
+    }
+}
