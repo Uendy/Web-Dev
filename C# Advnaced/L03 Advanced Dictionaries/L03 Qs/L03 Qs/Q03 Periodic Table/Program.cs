2e243a3e0953900343cb901336fc9c7f5586dbbb
Uendy <alex.beshkov98@gmail.com>
2021-01-18 11:53:22 +0200

    Adding: -Finished: Q03 Periodic Table

---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Qs/L03 Qs/Q03 Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading parameters:
+        var inputCount = int.Parse(Console.ReadLine());
+
+        // Intialize Hashset and fill:
+        var table = new SortedSet<string>();
+        for (int i = 0; i < inputCount; i++)
+        {
+            var elements = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var element in elements)
+            {
+                table.Add(element);
+            }
+        }
+
+        // Print all unique elements:
+        Console.WriteLine(string.Join(" ", table));
+    }
+}
