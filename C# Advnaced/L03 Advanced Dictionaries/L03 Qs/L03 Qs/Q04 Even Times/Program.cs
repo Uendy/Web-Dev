172d6176b37078d0f9a8605b0112edbda837e808
Uendy <alex.beshkov98@gmail.com>
2021-01-18 12:48:08 +0200

    Adding: -Tried: Q04 Even Times // Using GroupBy(), but it failed

---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Qs/L03 Qs/Q04 Even Times/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading inputCounts:
+        var inputCounts = int.Parse(Console.ReadLine());
+
+        // Initialize and fill data structure:
+        var list = new List<int>();
+        for (int i = 0; i < inputCounts; i++)
+        {
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+
+        // Sort and find the entry with odd counts:
+        var evenCount = list.GroupBy(x => x).Select(c => new { Key = c.Key, total = c.Count() % 2 == 0 }).First();
+        Console.WriteLine(evenCount.Key);
+    }
+}
