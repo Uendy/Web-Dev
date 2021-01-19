5189ffbef38ee4c47b36f8fd84b4f5e881fd2d15
Uendy <alex.beshkov98@gmail.com>
2021-01-19 19:01:45 +0200

    Adding: -Finished: Q05 Count Symbols

---
+++ C# Advnaced/L03 Advanced Dictionaries/L03 Qs/L03 Qs/Q05 Count Symbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        var input = Console.ReadLine().ToCharArray();
+
+        // Initialize dict: key = char, value = count:
+        var symbols = new SortedDictionary<char, int>();
+
+        // Cycle input and update dict:
+        for (int i = 0; i < input.Count(); i++)
+        {
+            var currentChar = input[i];
+            bool newSymbol = !symbols.ContainsKey(currentChar);
+            if (newSymbol)
+            {
+                symbols[currentChar] = 0;
+            }
+
+            symbols[currentChar]++;
+        }
+
+        // Print each char and its corresponding counts:
+        foreach (var symbol in symbols)
+        {
+            Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+        }
+    }
+}
