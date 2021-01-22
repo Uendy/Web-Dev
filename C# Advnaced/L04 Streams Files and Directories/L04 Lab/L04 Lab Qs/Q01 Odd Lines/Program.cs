252c4fb1dbd77970cb81201ee1f1ef615556b6d4
Uendy <alex.beshkov98@gmail.com>
2021-01-22 22:49:45 +0200

    Adding: -L04 Lab Q's folder: --Finished: Q01 Odd Lines

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Lab/L04 Lab Qs/Q01 Odd Lines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        var reader = new StreamReader("input.txt");
+        using (reader)
+        {
+            int index = 0;
+            while (true)
+            {
+                var line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                if (index % 2 != 0)
+                {
+                    Console.WriteLine(line);
+                }
+
+                index++;
+            }
+        }
+    }
+}
