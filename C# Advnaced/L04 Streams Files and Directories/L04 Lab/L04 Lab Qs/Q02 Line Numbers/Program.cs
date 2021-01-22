0bd06d1322bf43be0247eb62ca11fd472419dc73
Uendy <alex.beshkov98@gmail.com>
2021-01-22 23:13:25 +0200

    Adding: -Finished: Q02 Line Numbers

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Lab/L04 Lab Qs/Q02 Line Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        var reader = new StreamReader("input.txt");
+
+        int count = 1;
+        using (reader)
+        {
+            var line = reader.ReadLine();
+
+            var writer = new StreamWriter(".\\output.txt");
+            using (writer)
+            {
+                while (line != null)
+                {
+                    var numberedLine = $"{count}. {line}";
+                    writer.WriteLine(numberedLine);
+                    line = reader.ReadLine();
+                    count++;
+                }
+            }
+        }
+    }
+}
