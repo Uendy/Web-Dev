588de189a5cac7624bd59a16103cc54e49240b93
Uendy <alex.beshkov98@gmail.com>
2021-01-23 22:04:03 +0200

    Adding: -Finished: Q05 Slice File

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Lab/L04 Lab Qs/Q05 Slice File/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        var reader = new FileStream(@"..\..\input.txt", FileMode.OpenOrCreate);
+        using (reader)
+        {
+            var parts = 4;
+
+            // In case the number isn't divisable by parts, loosing data
+            var length = (int)Math.Ceiling(reader.Length / (decimal)parts);
+
+            var buffer = new byte[length];
+
+            for (int i = 0; i < parts; i++)
+            {
+                var bytesRead = reader.Read(buffer, 0, buffer.Length);
+
+                // In case the number isn't divisable by parts
+                if (bytesRead < buffer.Length)
+                {
+                    buffer = buffer.Take(bytesRead).ToArray();
+                }
+
+                var writer = new FileStream($@"..\..\Part{i + 1}.txt", FileMode.OpenOrCreate);
+                using (writer)
+                {
+                    writer.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
