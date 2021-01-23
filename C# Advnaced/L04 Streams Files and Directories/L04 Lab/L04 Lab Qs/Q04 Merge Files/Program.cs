0ed7ebe287be1d7639a11ed165898cd8581de669
Uendy <alex.beshkov98@gmail.com>
2021-01-23 21:26:25 +0200

    Adding: -Finished: Q04 Merge Files

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Lab/L04 Lab Qs/Q04 Merge Files/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        var firstFileLines = File.ReadAllLines(@"..\..\Input1.txt");
+        var secondFileLines = File.ReadAllLines(@"..\..\Input2.txt");
+
+        // This only work if both input files have the same amount of lines!
+        for (int i = 0; i < firstFileLines.Count(); i++)
+        {
+            File.AppendAllText(@"..\..\Output.txt", $"{firstFileLines[i]}\n");
+            File.AppendAllText(@"..\..\Output.txt", $"{secondFileLines[i]}\n");
+        }
+        
+    }
+}
