d9ef9c14ed7bfae95651935e8b9cc49009b12f84
Uendy <alex.beshkov98@gmail.com>
2021-01-23 21:09:12 +0200

    Adding: -Finished: Q03 Word Count

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Lab/L04 Lab Qs/Q03 Word Count/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        var text = File.ReadAllText(@"..\..\Input.txt")
+            .Split(new string[] { " ", "-", ".", "," }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.ToLower())
+            .ToArray();
+        var keywords = File.ReadAllText(@"..\..\words.txt")
+            .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.ToLower())
+            .ToArray();
+
+        var wordCount = new Dictionary<string, int>();
+
+        foreach (var word in text)
+        {
+            bool keyWordFound = keywords.Contains(word);
+            if (keyWordFound)
+            {
+                bool newWord = !wordCount.ContainsKey(word);
+                if (newWord)
+                {
+                    wordCount[word] = 0;
+                }
+                wordCount[word]++;
+            }
+        }
+
+        foreach (var word in wordCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value))
+        {
+            string output = $"{word.Key} - {word.Value}\r\n";
+            File.AppendAllText(@"..\..\Output.txt", output);
+        }
+        
+    }
+}
