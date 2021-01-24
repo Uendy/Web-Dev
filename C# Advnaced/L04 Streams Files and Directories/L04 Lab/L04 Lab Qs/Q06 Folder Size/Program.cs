58212cb3f633c1305aa19806b752aefe76c8c31a
Uendy <alex.beshkov98@gmail.com>
2021-01-24 22:03:57 +0200

    Adding: -Finished: Q06 Folder Size

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Lab/L04 Lab Qs/Q06 Folder Size/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        var folder = new DirectoryInfo(@"..\..\Test Folder");
+        var files = folder.GetFiles();
+
+        var totalLength = 0m;
+        foreach (var file in files)
+        {
+            totalLength += file.Length;
+        }
+
+        var megaBytes = totalLength / 1024 / 1024;
+        var output = $"{megaBytes:f4} MB";
+
+        File.WriteAllText("output.txt", output);
+    }
+}
