41a67f5f846ba02a6fdaf51e2ef4dd6f7342734f
Uendy <alex.beshkov98@gmail.com>
2021-01-25 18:21:11 +0200

    Adding: -L04 Qs folder: --Q01 Even Lines (Finished).
    // I hate everything and everyone and myself, because streams and files are killing me!

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Qs/L04 Qs/Q01 Even Lines/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        var lines = new List<string>();
+        var reader = new StreamReader(@".\text.txt");
+        using (reader)
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+                
+                lines.Add(line);
+            }
+        }
+
+        var output = new List<string>();
+        for (int i = 0; i < lines.Count(); i++)
+        {
+            bool isEven = i % 2 == 0;
+            if (isEven)
+            {
+                var currentLine = lines[i];
+                //{"-", ",", ".", "!", "?"}
+                currentLine = currentLine
+                    .Replace('-', '@')
+                    .Replace(',', '@')
+                    .Replace('.', '@')
+                    .Replace('!', '@')
+                    .Replace('?', '@');
+
+                currentLine = string.Join(" ",currentLine.Split(' ')
+                    .Reverse()
+                    .ToArray());
+                
+                output.Add(currentLine);
+            }
+        }
+
+        foreach (var line in output)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
