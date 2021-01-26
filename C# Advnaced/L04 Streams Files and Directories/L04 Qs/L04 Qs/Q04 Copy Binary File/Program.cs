6e38028359810d1fd889c84e938d6d5220989eeb
Uendy <alex.beshkov98@gmail.com>
2021-01-26 20:20:11 +0200

    Adding: -Finished: Q04 Copy Binary File (fully functioning code for copying)

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Qs/L04 Qs/Q04 Copy Binary File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+public class Program
+{
+    public static void Main()
+    {
+        const int defSize = 4096;
+
+        var reader = new FileStream("Bruno.jpg", FileMode.Open);
+
+        using (reader)
+        {
+            var buffer = new byte[defSize];
+            File.Delete("Copy.jpg");
+            var writer = new FileStream("Copy.jpg", FileMode.Create);
+            using (writer)
+            {
+                while (reader.CanRead)
+                {
+                    int bytesRead = reader.Read(buffer, 0, buffer.Length);
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    writer.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
