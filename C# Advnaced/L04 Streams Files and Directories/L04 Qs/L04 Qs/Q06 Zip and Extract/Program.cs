934f8554d1d6dafc4a87d5114af7a397a4c7c1f0
Uendy <alex.beshkov98@gmail.com>
2021-01-27 19:13:38 +0200

    Adding: -Q06 Zip and Extract // Having trouble with ZipFile class

---
+++ C# Advnaced/L04 Streams Files and Directories/L04 Qs/L04 Qs/Q06 Zip and Extract/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.IO.Compression;
+public class Program
+{
+    public static void Main()
+    {
+        // ZipFile.CreatFromDirectory(@$"{path and fileName}, {path and fileName}.zip");
+        // ZipFile.ExtractToDirectory(@$"{path and fileName}, {path}")
+    }
+}
