26a521826e1c053c381fbe64dad66591012b4934
Uendy <alex.beshkov98@gmail.com>
2021-01-28 19:13:39 +0200

    Adding: -L05 Lab Qs: --Finished: Q01 Sort Even Numbers

---
+++ C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q01 Sort Even Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        var input = Console.ReadLine()
+            .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        // Execute lambda expression:
+        var evenNums = input
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x)
+            .ToList();
+
+        // Printing output:
+        Console.WriteLine(string.Join(", ", evenNums));
+    }
+}
