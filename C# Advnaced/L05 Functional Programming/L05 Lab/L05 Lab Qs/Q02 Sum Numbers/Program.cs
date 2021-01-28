eae8d666979892f6cd113ec2ff30bec3b0603722
Uendy <alex.beshkov98@gmail.com>
2021-01-28 20:32:06 +0200

    Adding: -Finished: Q02 Sum Numbers

---
+++ C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q02 Sum Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading input:
+        var input = Console.ReadLine()
+            .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        // Printing count and sum:
+        Console.WriteLine(input.Count());
+        Console.WriteLine(input.Sum());
+    }
+}
