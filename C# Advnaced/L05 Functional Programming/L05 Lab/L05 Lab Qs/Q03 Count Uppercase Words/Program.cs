ceec1eae8e166adc69ded1bbbb46838f1ba9ae06
Uendy <alex.beshkov98@gmail.com>
2021-01-29 19:25:00 +0200

    Adding: -Q03 Count Uppercase Words

---
+++ C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q03 Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        Func<string, bool> capitalizationChecker = n => n[0] == n.ToUpper()[0];
+
+        var input = Console.ReadLine()
+            .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+            .Where(capitalizationChecker)
+            .ToList();
+
+        foreach (var word in input)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
