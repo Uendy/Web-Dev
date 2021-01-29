b48643007e35758eabf1679324995024f281c555
Uendy <alex.beshkov98@gmail.com>
2021-01-29 19:36:14 +0200

    Adding: -Finished: Q04 Add Vat

---
+++ C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q04 Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        Func<string, double> doubleParser = x => double.Parse(x);
+        Func<double, double> addVat = x => x * 1.2;
+
+        var input = Console.ReadLine()
+            .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(doubleParser)
+            .Select(addVat)
+            .ToList();
+
+        input.Select(addVat);
+
+        foreach (var price in input)
+        {
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
