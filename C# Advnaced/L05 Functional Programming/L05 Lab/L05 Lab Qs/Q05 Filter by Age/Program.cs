8b177586017da17b256ac21831023c570f3168fc
Uendy <alex.beshkov98@gmail.com>
2021-01-30 19:28:11 +0200

    Adding: -Q05 Filter by Age (I am so confused, since I cant use the 8.0 syntax)

---
+++ C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q05 Filter by Age/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Reading inputs:
+        var inputs = int.Parse(Console.ReadLine());
+
+        var listOfPeople = new List<Person>();
+        for (int i = 0; i < inputs; i++)
+        {
+            var input = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var person = new Person()
+            {
+                Name = input[0],
+                Age = int.Parse(input[1])
+            };
+
+            listOfPeople.Add(person);
+        }
+
+
+        var ageFilter = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+
+        Func<int, bool> tester = CheckAge(ageFilter, age);
+
+        listOfPeople.Where(CheckAge).ToList();
+    }
+
+    public static Func<int, bool> CheckAge(string agefilter, int age)
+    {
+        switch (agefilter)
+        {
+            case "younger": return x => x < age;
+            case "older": return x => x >= age;
+            default: return null;
+        }
+    }
+}
