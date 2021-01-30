5d16c9d70ff58270e8a5e89a154368a1bcd9af0d
Uendy <alex.beshkov98@gmail.com>
2021-01-30 22:34:31 +0200

    Update: -Finished Q05 Filter by Age
    // This took me ages and so many nerves to do, I am still really confused about the syntax and how it is applied, but atleast I have 2 archetypal examples

--- C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q05 Filter by Age/Program.cs
+++ C# Advnaced/L05 Functional Programming/L05 Lab/L05 Lab Qs/Q05 Filter by Age/Program.cs
@@ -5,39 +5,65 @@ public class Program
 {
     public static void Main()
     {
-        // Reading inputs:
-        var inputs = int.Parse(Console.ReadLine());
-
+        var inputCount = int.Parse(Console.ReadLine());
         var listOfPeople = new List<Person>();
-        for (int i = 0; i < inputs; i++)
+        for (int i = 0; i < inputCount; i++)
         {
-            var input = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-            var person = new Person()
+            var input = Console.ReadLine().Split(new string[] { ", "}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var person = new Person
             {
                 Name = input[0],
                 Age = int.Parse(input[1])
             };
-
             listOfPeople.Add(person);
         }
 
-
-        var ageFilter = Console.ReadLine();
+        var youngerOrOlder = Console.ReadLine();
         var age = int.Parse(Console.ReadLine());
+        var format = Console.ReadLine();
 
-        Func<int, bool> tester = CheckAge(ageFilter, age);
+        Func<int, bool> tester = CreateTester(youngerOrOlder, age);
+        Action<Person> writer = CreateWriter(format);
 
-        listOfPeople.Where(CheckAge).ToList();
+        foreach (var person in listOfPeople)
+        {
+            if (tester(person.Age))
+            {
+                writer(person);
+            }
+        }
     }
 
-    public static Func<int, bool> CheckAge(string agefilter, int age)
+    public static Action<Person> CreateWriter(string format)
     {
-        switch (agefilter)
+        switch (format)
         {
-            case "younger": return x => x < age;
-            case "older": return x => x >= age;
-            default: return null;
+            case "name":
+                return x => Console.WriteLine($"{x.Name}");
+
+            case "age":
+                return x => Console.WriteLine($"{x.Age}");
+
+            case "name age":
+                return x => Console.WriteLine($"{x.Name} - {x.Age}");
+
+            default:
+                return null;
+        }
+    }
+
+    public static Func<int, bool> CreateTester(string youngerOrOlder, int age)
+    {
+        switch (youngerOrOlder)
+        {
+            case "younger":
+                return x => x < age;
+
+            case "older":
+                return x => x >= age;
+
+            default:
+                return null;
         }
     }
 }
