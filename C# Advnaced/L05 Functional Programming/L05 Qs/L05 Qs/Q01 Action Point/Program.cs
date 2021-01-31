f8aab91946de6396f450fc7a4844c3597a5350ad
Uendy <alex.beshkov98@gmail.com>
2021-01-31 22:04:08 +0200

    Update: -Finished Q01 Action Point

--- C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q01 Action Point/Program.cs
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q01 Action Point/Program.cs
@@ -5,5 +5,10 @@ public class Program
 {
     public static void Main()
     {
+        var inputs = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        Action <string> print = x => Console.WriteLine(x);
+
+        inputs.ForEach(x => print(x));
     }
 }
