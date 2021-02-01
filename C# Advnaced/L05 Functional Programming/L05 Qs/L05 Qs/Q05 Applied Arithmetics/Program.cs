f0d26dbd25d41236d8298493b07d48396d52239b
Uendy <alex.beshkov98@gmail.com>
2021-02-01 20:42:14 +0200

    Adding: -Finished: Q05 Applied Arithmetics

---
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q05 Applied Arithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var list = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        string command;
+        while ((command = Console.ReadLine()) != "end")
+        {
+            if (command == "print")
+            {
+                Action<List<int>> Printer = Print(list);
+                Printer(list);
+            }
+            else
+            {
+                Func<int, int> MyFunc = ManipulateList(command);
+                list = list.Select(x => MyFunc(x)).ToList();
+            }
+        }
+    }
+
+    public static Action<List<int>> Print(List<int> list)
+    {
+        return x => Console.WriteLine(string.Join(" ", x));
+    }
+
+    public static Func<int, int> ManipulateList(string command)
+    {
+        switch (command)
+        {
+            case "add":
+                return x => x + 1;
+
+            case "subtract":
+                return x => x - 1;
+
+            case "multiply":
+                return x => x * 2;
+
+            default:
+                return x => x;
+        }
+    }
+}
