32bd9592a6a1a837ff483c49c07da7032e263f93
Uendy <alex.beshkov98@gmail.com>
2021-02-01 18:27:53 +0200

    Adding: -Finished: Q03 Custom Min Function

---
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q03 Custom Min Function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var list = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        Func<List<int>, int> customMin = FindMin;
+
+        var result = customMin(list);
+        Console.WriteLine(result);
+    }
+    public static int FindMin(List<int> list)
+    {
+        int min = int.MaxValue;
+        foreach (var item in list)
+        {
+            if (item < min)
+            {
+                min = item;
+            }
+        }
+
+        return min;
+    }
+}
