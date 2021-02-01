0c65ef9d922b81f9a45f5bfaa5a67fad6d7efbd7
Uendy <alex.beshkov98@gmail.com>
2021-02-01 20:01:13 +0200

    Update: -Finished: Q04 Find Evens or Odds

--- C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q04 Find Evens or Odds/Program.cs
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q04 Find Evens or Odds/Program.cs
@@ -5,12 +5,20 @@ public class Program
     public static void Main()
     {
         var list = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        var start = list[0];
+        var end = list[1];
+
+        // Getting values between first and last:
+        for (int i = start; i < end; i++)
+        {
+            list.Insert(list.Count() - 1, i);
+        }
 
         string oddOrEven = Console.ReadLine().ToLower();
 
         Func<int, bool> FilterFunc = FilterList(oddOrEven);
 
-        list.Where(x => FilterFunc(x)).ToList();
+        list = list.Where(x => FilterFunc(x)).ToList();
         Console.WriteLine(string.Join(" ", list));
     }
 
@@ -25,7 +33,7 @@ public class Program
                 return x => x % 2 == 0;
                 
             default:
-                return null;
+                return x => x == x;
         }
     }
 }
