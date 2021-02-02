c6a9108632fe9aead1821b8e3cfac8bfef42888c
Uendy <alex.beshkov98@gmail.com>
2021-02-02 20:49:19 +0200

    Adding: -Finished: Q07 Predicate of Names

---
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q07 Predicate of Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var length = int.Parse(Console.ReadLine());
+        Func<string, bool> checkLength = CheckLength(length);
+
+        var names = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        names = names.Where(x => checkLength(x)).ToList();
+
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+    public static Func<string, bool> CheckLength(int length)
+    {
+        return x => x.Length <= length;
+    }
+}
