aef2e4f1854dc378ee55538b08e4c672a8e13a9c
Uendy <alex.beshkov98@gmail.com>
2021-02-02 18:58:24 +0200

    Update: -Finished: Q06 Reverse and Exclude

--- C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q06 Reverse and Exclude/Program.cs
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q06 Reverse and Exclude/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Q06_Reverse_and_Exclude
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        var list = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        // To Revese the list, without using .Reverse()
+        Func<List<int>, List<int>> Reverser = ReverseList(list);
+        list = Reverser(list);
+
+        // To exclude divisable numbers
+        var denominator = int.Parse(Console.ReadLine());
+        Func<int, bool> Excludor = ExcludeDivisableElements(denominator);
+        list = list.Where(x => Excludor(x) != true).ToList();
+
+        Console.WriteLine(string.Join(" ", list));
+    }
+    public static Func<List<int>, List<int>> ReverseList(List<int> list)
+    {
+        var newList = new List<int>();
+        for (int i = list.Count() - 1; i >= 0; i--)
         {
+            newList.Add(list[i]);
         }
+
+        return x => newList;
+    }
+    public static Func<int, bool> ExcludeDivisableElements(int denominator)
+    {
+        return x => x % denominator == 0;
     }
 }
