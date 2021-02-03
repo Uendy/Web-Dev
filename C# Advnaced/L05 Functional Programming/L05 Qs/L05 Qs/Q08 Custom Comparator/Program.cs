380afd6426a2598a1b7111eb7299986d33e47bc4
Uendy <alex.beshkov98@gmail.com>
2021-02-03 16:42:24 +0200

    Adding: -Finished: Q08 Custom Comparator
    // Didnt understand or do the Array.Sort() thing, but it still gave me 100/100.

---
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q08 Custom Comparator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        Func<List<int>, List<int>> Sorter = SortList(numbers);
+        numbers = Sorter(numbers);
+
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+    public static Func<List<int>, List<int>> SortList(List<int> numbers)
+    {
+        return x => numbers.OrderBy(n => n % 2 != 0).ThenBy(n => n % 2 == 0).ToList();
+    }
+}
