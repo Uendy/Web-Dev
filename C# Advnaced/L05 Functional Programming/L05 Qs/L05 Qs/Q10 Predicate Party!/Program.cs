1fd538806a72e8e4a496562d509e8e7321596162
Uendy <alex.beshkov98@gmail.com>
2021-02-04 20:18:59 +0200

    Adding: -Q10 Predicate Party! (start)
    // Misunderstood the question, have to re-do it a bit.

---
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q10 Predicate Party!/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var guests = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        
+
+        string line;
+        while ((line = Console.ReadLine()) != "Party")
+        {
+            var inputTokens = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            string command = inputTokens[1];
+            string predicate = inputTokens[2];
+            
+            Func<string, bool> Function = ManipulateGuestList(command, predicate);
+        }
+    }
+
+    public static Func<string, bool> ManipulateGuestList(string command, string predicate)
+    {
+        switch (command)
+        {
+            case "StartsWith":
+                return x => x.Where(x.First() == "predicate");
+                    break;
+
+            case "EndsWith":
+
+                break;
+
+            case "Length":
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
