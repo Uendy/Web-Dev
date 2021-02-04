933839ac69f08c233c7389ff5d5dfb1ae84b6692
Uendy <alex.beshkov98@gmail.com>
2021-02-04 20:49:53 +0200

    Finished: -Q10 Predicate Party!

--- C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q10 Predicate Party!/Program.cs
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q10 Predicate Party!/Program.cs
@@ -7,37 +7,63 @@ public class Program
     {
         var guests = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
 
-        
-
         string line;
-        while ((line = Console.ReadLine()) != "Party")
+        while ((line = Console.ReadLine()) != "Party!")
         {
             var inputTokens = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
-            string command = inputTokens[1];
-            string predicate = inputTokens[2];
-            
-            Func<string, bool> Function = ManipulateGuestList(command, predicate);
+            var command = inputTokens[0];
+            var filter = inputTokens[1];
+            var predicate = inputTokens[2];
+
+            ManipulateGuestList(guests, command, filter, predicate);
+        }
+
+        if (guests.Any())
+        {
+            Console.WriteLine($"{ string.Join(" ", guests.OrderBy(x => x))} are going to the party!");
+        }
+        else
+        {
+            Console.WriteLine("Nobody is going to the party!");
         }
     }
 
-    public static Func<string, bool> ManipulateGuestList(string command, string predicate)
+    public static void ManipulateGuestList(List<string> guests, string command, string filter, string predicate)
     {
-        switch (command)
+        switch (filter)
         {
             case "StartsWith":
-                return x => x.Where(x.First() == "predicate");
-                    break;
-
+                ForeachName(command, guests, n => n.StartsWith(predicate));
+                break;
             case "EndsWith":
-
+                ForeachName(command, guests, n => n.EndsWith(predicate));
                 break;
-
             case "Length":
-
+                ForeachName(command, guests, n => n.Length == int.Parse(predicate));
                 break;
-
             default:
                 break;
+        }
+    }
+
+    public static void ForeachName(string command, List<string> guests, Func<string, bool> condition)
+    {
+        for (int i = guests.Count - 1; i >= 0; i--)
+        {
+            if (condition(guests[i]))
+            {
+                switch (command)
+                {
+                    case "Remove":
+                        guests.RemoveAt(i);
+                        break;
+                    case "Double":
+                        guests.Add(guests[i]);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
