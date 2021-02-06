c932dc1527bf398e744a67715e03beabdd789187
Uendy <alex.beshkov98@gmail.com>
2021-02-06 22:06:49 +0200

    Finished: -Q12 TriFunction

--- C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q12 TriFunction/Program.cs
+++ C# Advnaced/L05 Functional Programming/L05 Qs/L05 Qs/Q12 TriFunction/Program.cs
@@ -5,5 +5,16 @@ public class Program
 {
     public static void Main()
     {
+        var targetCharactersSum = int.Parse(Console.ReadLine());
+        var names = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray()
+            .Select(ch => (int)ch).Sum() >= num;
+
+        Func<List<string>, int, Func<string, int, bool>, string> firstValidName = (list, num, func) => list
+            .FirstOrDefault(str => func(str, num));
+
+        var result = firstValidName(names, targetCharactersSum, isValidWord);
+        Console.WriteLine(result);
     }
 }
