296bade5c4acb755a8d720780807ba6da51bef58
Uendy <alex.beshkov98@gmail.com>
2021-02-07 19:45:07 +0200

    Adding: -L06 Lab Qs folder: --Q01 Car (Finished)

---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q01 Car/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var car = new Car();
+
+        car.Make = "VW";
+        car.Model = "MK3";
+        car.Year = 1993;
+
+        Console.WriteLine($"Make: {car.Make}");
+        Console.WriteLine($"Model: {car.Model}");
+        Console.WriteLine($"Year: {car.Year}");
+    }
+}
