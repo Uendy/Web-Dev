7b299f623d680ad839d53ea0d1fc82b6181df58f
Uendy <alex.beshkov98@gmail.com>
2021-02-07 20:44:25 +0200

    Adding: -Finished: Q02 Car Extension

---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q02 Car Extension/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public double FuelQuantity { get; set; }
+    public double FuelConsumption { get; set; }
+
+    public void Drive(double distance)
+    {
+        bool enoughFuel = this.FuelQuantity > this.FuelConsumption * distance;
+        if (enoughFuel)
+        {
+            this.FuelQuantity -= (this.FuelConsumption * distance);
+        }
+        else
+        {
+            Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+    }
+
+    public string WhoAmI()
+    {
+        return $"Make: {this.Make} \nModel: {this.Model} \nYear: {this.Year} \nFuel: {this.FuelConsumption:f2}";
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q02 Car Extension/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var car = new Car();
+
+        car.Make = "VW";
+        car.Model = "MK3";
+        car.Year = 1992;
+        car.FuelQuantity = 200;
+        car.FuelConsumption = 200;
+        car.Drive(2000);
+        Console.WriteLine(car.WhoAmI());
+    }
+}
