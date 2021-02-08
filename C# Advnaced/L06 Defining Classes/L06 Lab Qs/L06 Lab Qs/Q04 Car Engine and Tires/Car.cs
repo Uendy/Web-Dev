4b556973c0ae6272fe297df20e2db207bae4064d
Uendy <alex.beshkov98@gmail.com>
2021-02-08 21:45:37 +0200

    Adding: -Finished: Q04 Car Engine and Tires

---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q04 Car Engine and Tires/Car.cs
@@ -0,0 +1,36 @@
+﻿public class Car
+{
+    public string Make { get; }
+    public string Model { get; }
+    public int Year { get; }
+    public double FuelQuantity { get; set; }
+    public double FuelConsumption { get; set; }
+    public Engine Engine { get; }
+    public Tire[] Tires { get; set; }
+
+    public Car()
+    { 
+    
+    }
+
+    public Car(string make, string model, int year)
+    {
+        this.Make = make;
+        this.Model = model;
+        this.Year = year;
+    }
+
+    public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        :this(make, model, year)
+    {
+        this.FuelQuantity = fuelQuantity;
+        this.FuelConsumption = fuelConsumption;
+    }
+
+    public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        : this(make, model, year, fuelQuantity, fuelConsumption)
+    {
+        this.Engine = engine;
+        this.Tires = tires;
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q04 Car Engine and Tires/Engine.cs
@@ -0,0 +1,11 @@
+﻿public class Engine
+{
+    public int HorsePower { get; }
+    public double CubicCapacity { get; }
+
+    public Engine(int horsePower, double cubicCapacity)
+    {
+        this.HorsePower = horsePower;
+        this.CubicCapacity = cubicCapacity;
+    }
+}
