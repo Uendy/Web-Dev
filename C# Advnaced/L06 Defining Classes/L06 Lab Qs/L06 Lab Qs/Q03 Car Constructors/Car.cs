25b0f0c6f372e8ee7eaf244b2b276645ffb2fa48
Uendy <alex.beshkov98@gmail.com>
2021-02-08 21:21:54 +0200

    Adding: -Finished: Q03 Car Constructors

---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q03 Car Constructors/Car.cs
@@ -0,0 +1,92 @@
+﻿public class Car
+{
+    // Initialize Fields:
+    private string make;
+    private string model;
+    private int year;
+    private double fuelQuantity;
+    private double fuelConsumption;
+
+    // Fill public properties
+    //public string Make
+    //{
+    //    get
+    //    {
+    //        return this.make;
+    //    }
+    //    set
+    //    {
+    //        this.make = value;
+    //    }
+    //}
+
+    //public string Model
+    //{
+    //    get
+    //    {
+    //        return this.model;
+    //    }
+    //    set
+    //    {
+    //        this.model = value;
+    //    }
+
+    //}
+
+    //public int Year
+    //{
+    //    get
+    //    {
+    //        return this.year;
+    //    }
+    //    set
+    //    {
+    //        this.year = value;
+    //    }
+    //}
+
+    //public double FuelQuantity
+    //{
+    //    get
+    //    {
+    //        return this.fuelQuantity;
+    //    }
+    //    set
+    //    {
+    //        this.fuelQuantity = value;
+    //    }
+    //}
+
+    //public double FuelConsumption
+    //{
+    //    get
+    //    {
+    //        return this.fuelConsumption;
+    //    }
+    //    set
+    //    {
+    //        this.fuelConsumption = value;
+    //    }
+    //}
+
+    // Constructors:
+
+    public Car()
+    { 
+    
+    }
+
+    public Car(string Make, string Model, int Year)
+    {
+        this.make = Make;
+        this.model = Model;
+        this.year = Year;
+    }
+
+    public Car(string Make, string Model, int Year, double FuelQuantity, double FuelConsumption)
+        : this(Make, Model, Year)
+    {
+        this.fuelQuantity = FuelQuantity;
+        this.fuelConsumption = FuelConsumption;
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Lab Qs/L06 Lab Qs/Q03 Car Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        string make = Console.ReadLine();
+        string model = Console.ReadLine();
+        int year = int.Parse(Console.ReadLine());
+        double fuelQuantity = double.Parse(Console.ReadLine());
+        double fuelConsumption = double.Parse(Console.ReadLine());
+
+        var firstCar = new Car();
+        var secondCar = new Car(make, model, year);
+        var thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+    }
+}
