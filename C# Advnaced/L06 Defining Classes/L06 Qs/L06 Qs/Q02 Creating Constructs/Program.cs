1335b135385f250e46f61bac0dc4ab1a360e441f
Uendy <alex.beshkov98@gmail.com>
2021-02-09 18:18:22 +0200

    Adding: -Finished: Q02 Creating Constructs

---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q02 Creating Constructs/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var first = new Person();
+        var second = new Person(15);
+        var third = new Person("Sven", 15);
+    }
+}
