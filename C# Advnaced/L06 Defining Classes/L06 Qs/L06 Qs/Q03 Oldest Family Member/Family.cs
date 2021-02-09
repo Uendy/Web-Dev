a15216185bcdf1e910184b19dc8046333d24f22f
Uendy <alex.beshkov98@gmail.com>
2021-02-09 19:15:50 +0200

    Adding: -Finished: Q03 Oldest Family Member

---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q03 Oldest Family Member/Family.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+public class Family
+{
+    public List<Person> members = new List<Person>();
+
+    public void AddMember(Person member)
+    {
+        members.Add(member);
+    }
+
+    public Person ReturnOlder()
+    {
+        return members.OrderByDescending(x => x.Age).First();
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q03 Oldest Family Member/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var inputCounts = int.Parse(Console.ReadLine());
+
+        var family = new Family();
+        for (int i = 0; i < inputCounts; i++)
+        {
+            var currentMemberInfo = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var name = currentMemberInfo[0];
+            var age = int.Parse(currentMemberInfo[1]);
+
+            var member = new Person(name, age);
+            family.AddMember(member);
+        }
+
+        var olderMember = family.ReturnOlder();
+        Console.WriteLine($"{olderMember.Name} {olderMember.Age}");
+    }
+}
