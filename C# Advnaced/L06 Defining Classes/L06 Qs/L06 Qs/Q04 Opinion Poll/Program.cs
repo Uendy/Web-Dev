34e6c84c1643f62cd940a50ff85cf80c2a246f5a
Uendy <alex.beshkov98@gmail.com>
2021-02-10 21:35:45 +0200

    Adding: -Finished: Q04 Opinion Poll

---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q04 Opinion Poll/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var inputCount = int.Parse(Console.ReadLine());
+        var people = new List<Person>();
+        for (int i = 0; i < inputCount; i++)
+        {
+            var personInfo = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var name = personInfo[0];
+            var age = int.Parse(personInfo[1]);
+
+            var person = new Person(name, age);
+            people.Add(person);
+        }
+
+        var selectedPeople = people.Where(x => x.Age >= 30).OrderBy(y => y.Name).ToList();
+        foreach (var person in selectedPeople)
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
