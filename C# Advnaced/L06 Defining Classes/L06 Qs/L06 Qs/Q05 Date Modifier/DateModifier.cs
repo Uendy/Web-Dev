61070fb951afc96f8941d9f992373854e785107a
Uendy <alex.beshkov98@gmail.com>
2021-02-10 21:55:50 +0200

    Adding: -Finished: Q05 Date Modifier
    // Forgot how annoying DateTime formatting is

---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q05 Date Modifier/DateModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+public class DateModifier
+{
+    public string FirstDate { get; set; }
+    public string SecondDate { get; set; }
+
+    public DateModifier(string firstDate, string secondDate)
+    {
+        this.FirstDate = firstDate;
+        this.SecondDate = secondDate;
+    }
+
+    public double DayDifference(string firstDate, string secondDate)
+    {
+        var firstDateTime = DateTime.ParseExact(firstDate, "yyyy MM dd", CultureInfo.InvariantCulture);
+        var secondDateTime = DateTime.ParseExact(secondDate, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+        var interval = firstDateTime - secondDateTime;
+        var days = Math.Abs(interval.TotalDays);
+        return days;
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q05 Date Modifier/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var firstDate = Console.ReadLine();
+        var secondDate = Console.ReadLine();
+
+        var dateTime = new DateModifier(firstDate, secondDate);
+
+        Console.WriteLine(dateTime.DayDifference(dateTime.FirstDate, dateTime.SecondDate));
+    }
+}
