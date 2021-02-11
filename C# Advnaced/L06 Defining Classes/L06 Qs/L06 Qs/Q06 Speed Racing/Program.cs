0cb6fe17a4b8e815905bd979ce191515e5241147
Uendy <alex.beshkov98@gmail.com>
2021-02-11 22:35:23 +0200

    Update: -Finished: Q06 Speed Racing

--- C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q06 Speed Racing/Program.cs
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q06 Speed Racing/Program.cs
@@ -5,6 +5,40 @@ public class Program
 {
     public static void Main()
     {
+        var inputCount = int.Parse(Console.ReadLine());
+        var cars = new List<Car>();
 
+        for (int i = 0; i < inputCount; i++)
+        {
+            var currentCarInfo = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var model = currentCarInfo[0];
+            var fuelAmount = double.Parse(currentCarInfo[1]);
+            var fuelConsumption = double.Parse(currentCarInfo[2]);
+
+            var currentCar = new Car(model, fuelAmount, fuelConsumption);
+
+            cars.Add(currentCar);
+        }
+
+        string driveInput;
+        while ((driveInput = Console.ReadLine()) != "End")
+        {
+            var driveInputInfo = driveInput.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var model = driveInputInfo[1];
+            var distance = int.Parse(driveInputInfo[2]);
+
+            bool carExists = cars.Any(x => x.Model == model);
+            if (carExists)
+            {
+                var currentCar = cars.Where(x => x.Model == model).First();
+
+                currentCar.DriveCar(distance);
+            }
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+        }
     }
 }
