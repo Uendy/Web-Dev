d4d0df5571709d1e0479e7750b8206dd0fd901b0
Uendy <alex.beshkov98@gmail.com>
2021-02-12 21:07:24 +0200

    Adding: -Q07 Raw Data (start)

---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q06 Speed Racing/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Car
+{
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelConsumptionPerKilometer { get; set; }
+    public double TravelledDistance { get; set; }
+
+    public Car(string model, double fuelAMount, double fuelConsumptionPerKilometer)
+    {
+        this.Model = model;
+        this.FuelAmount = fuelAMount;
+        this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        this.TravelledDistance = 0;
+    }
+
+    public void DriveCar(double distance)
+    {
+        var fuelNeeded = distance * FuelConsumptionPerKilometer;
+        bool enoughFuel = FuelAmount >= fuelNeeded;
+        if (enoughFuel)
+        {
+            FuelAmount -= fuelNeeded;
+            TravelledDistance += distance;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q07 Raw Data/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q07_Raw_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
