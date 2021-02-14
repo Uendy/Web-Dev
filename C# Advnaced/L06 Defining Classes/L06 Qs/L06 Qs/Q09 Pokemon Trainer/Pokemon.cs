2993860644bea77f03d4550a7168c5c7454f0510
Uendy <alex.beshkov98@gmail.com>
2021-02-14 20:17:48 +0200

    Adding: -Q09 Pokemon Trainer (start)
    // Initialized classes for Trainer and Pokemon

---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q09 Pokemon Trainer/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿public class Pokemon
+{
+    public string Name { get; set; }
+    public int HP { get; set; }
+    public string Element { get; set; }
+
+    public Pokemon(string name, int hp, string element)
+    {
+        this.Name = name;
+        this.HP = hp;
+        this.Element = element;
+    }
+}
---
+++ C# Advnaced/L06 Defining Classes/L06 Qs/L06 Qs/Q09 Pokemon Trainer/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class Trainer
+{
+    public string Name { get; set; }
+    public int Badges { get; set; }
+    public List<Pokemon> Pokemons { get; set; }
+
+    public Trainer(string name, int badges, List<Pokemon> pokemons)
+    {
+        this.Name = name;
+        this.Badges = badges;
+        this.Pokemons = pokemons;
+    }
+}
